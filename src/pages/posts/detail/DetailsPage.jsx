f0feb3a470eb34c791a92b11e4bf65590185ff7d
Dani-Davydov <hudad1046@gmail.com>
2025-05-20 18:24:04 +0300

    Create detail post page + new ui component Link

--- src/pages/posts/detail/DetailsPage.jsx
+++ src/pages/posts/detail/DetailsPage.jsx
@@ -1,7 +1,30 @@
+import { useParams} from "react-router-dom";
+import {Link} from "../../../components/Link/Link.jsx";
+import { initialPosts } from "../PostsPage.jsx";
+import {useMemo} from "react";
+import {Typo} from "../../../components/Typo/Typo.jsx";
+import {Container} from "../../../components/Container/index.jsx";
+import * as SC from "./styles.js"
+import {LinkWrapper} from "./styles.js";
+
 export  const DetailPostPage = () => {
+    const { id } = useParams();
+    const currentPost = useMemo(() => initialPosts.find((post) => post.id === Number(id)), [id])
+
+    if (!currentPost) {
+        return <>Пост не найден</>;
+    }
+
     return (
-        <div>
-            детальная стр постов
-        </div>
+        <Container>
+            <Typo>{currentPost.title}</Typo>
+            <br/>
+            <SC.Image src={currentPost.image} alt={currentPost.title}></SC.Image>
+            <SC.Text>{currentPost.text}</SC.Text>
+            <div style={{ clear: 'both' }}></div>
+            <LinkWrapper>
+                <Link to={'/posts'}>Обратно на страницу постов</Link>
+            </LinkWrapper>
+        </Container>
     )
 }
---
+++ src/pages/posts/detail/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+export const Image = styled.img`
+        width: 150px;
+        height: 150px;
+        float: left;
+        margin-right: 15px;
+`
+export const Text = styled.div`
+    font-size: 15px;
+`
+
+export const LinkWrapper = styled.div`
+    text-align: center;
+    margin-top: 15px;
+`
